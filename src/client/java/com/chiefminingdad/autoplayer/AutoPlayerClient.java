54b4ec051757ee55bf986d327eed7c6c6dec5b1b
Micah Butt <micahbutt@outlook.com>
2026-08-31 21:47:21 +0100

    First Commit

---
+++ src/client/java/com/chiefminingdad/autoplayer/AutoPlayerClient.java
@@ -0,0 +1,10 @@
+package com.chiefminingdad.autoplayer;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutoPlayerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
