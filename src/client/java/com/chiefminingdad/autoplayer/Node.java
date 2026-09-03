1677749bad8988e856bf3d82b7dbe5d3ca2769a2
Micah Butt <micahbutt@outlook.com>
2026-09-03 17:39:04 +0100

    Node setup

--- src/client/java/com/chiefminingdad/autoplayer/Node.java
+++ src/client/java/com/chiefminingdad/autoplayer/Node.java
@@ -1,19 +1,17 @@
 package com.chiefminingdad.autoplayer;
 
+import net.minecraft.core.Vec3i;
 import net.minecraft.world.phys.Vec3;
 
 public class Node {
-    Vec3 Location;
-    int Index;
-    int PreviousIndex;
+    public Vec3i Location;
+    public Vec3i PreviousLocation;
 
-    public Node(Vec3 location,int index , int previousIndex){
+    public Node(Vec3i location, int index , Vec3i previousLocation){
         Location = location;
-        Index = index;
-        PreviousIndex = previousIndex;
+        PreviousLocation = previousLocation;
     }
-    public Node(Vec3 location,int index){
+    public Node(Vec3i location,int index){
         Location = location;
-        Index = index;
     }
 }
--- src/client/java/com/chiefminingdad/autoplayer/NodeManager.java
+++ src/client/java/com/chiefminingdad/autoplayer/NodeManager.java
@@ -1,17 +1,34 @@
 package com.chiefminingdad.autoplayer;
 
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
 import net.minecraft.world.phys.Vec3;
+import org.jspecify.annotations.NonNull;
 
+import java.util.ArrayList;
 import java.util.Dictionary;
 import java.util.Hashtable;
 
 public class NodeManager {
-    Dictionary<Vec3, Integer> ReferenceTable = new Hashtable<>();
+    Dictionary<Vec3i, Integer> ReferenceTable = new Hashtable<>();
 
+    ArrayList<Node> NodeList = new ArrayList<>();
 
+    public NodeManager(Node first){
+        NodeList.add(first);
+        ReferenceTable.put(first.Location,0);
+    }
 
-    NodeManager(){
+    public void addNode(@NonNull Node newNode){
+        ReferenceTable.put(newNode.Location,NodeList.size());
+        NodeList.add(newNode);
+    }
 
+    public void updateIndexes(){
+        for(int i =0; i<NodeList.size();i++){
+            Vec3i loc = NodeList.get(i).Location;
+            ReferenceTable.put(loc,i);
+        }
     }
 
     public int getNodeIndex(Vec3 vector){
